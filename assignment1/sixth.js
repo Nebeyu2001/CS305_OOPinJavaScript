acdc70f67c4432e25e0ed9ef7fadd6116d5ce076
Nebeyu Z <64758297+Nebeyu2001@users.noreply.github.com>
2022-02-21 21:57:47 -0600

    w1d1

---
+++ assignment1/sixth.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")();
+
+let userInput = prompt("please enter the number ")
+
+let result = 0;
+
+for (let i = 0; i < userInput.length; i++) {
+
+    result = result + i;
+
+}
+//
+console.log(result);
+
+const prompt = require("prompt-sync")();
+
+let userInput = prompt("please enter the number ")
+
+let result = 0;
+
+for (let i = 0; i < userInput.length; i++) {
+
+    result = result + Number(userInput.charAt(i));
+
+}
+
+console.log(result);
